5b044410921b2cf1e9c8f3bc0f3e0c0b7be980b4
tjasw549 <147879459+tjasw549@users.noreply.github.com>
2023-10-13 19:08:19 -0600

    Add files via upload

--- lifeJourney/FinalGame.java
+++ lifeJourney/FinalGame.java
@@ -3,11 +3,25 @@ package lifeJourney;
 public class FinalGame {
 
     public static void main(String[] args) {
+    	
+    	System.out.println("Welcome to ?");
+    	System.out.println(""); //description
 
-
+    	System.out.println("You are starting as a Toddler");
+        System.out.println("Starting Game: Match the Shape..." + '\n');
         MatchTheShape mtsGame = new MatchTheShape();
+        mtsGame.startGame();
+        
+        System.out.println("You have grown up");
+        System.out.println("You are now a Child");
+        System.out.println("Starting Game: Math Homework..." + '\n');
+        ChildMath mathGame = new ChildMath();
+        mathGame.startGame();
+        
+        System.out.println("You have grown up");
+        System.out.println("You are now a Teenager");
+        System.out.println("Starting Game: Match the Shape..." + '\n');
         TeenageHood teenGame = new TeenageHood();
-       // mtsGame.startGame();
         teenGame.startGame();
 
 
